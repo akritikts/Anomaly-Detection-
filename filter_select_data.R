d8734c5a4783b7216fc5627bef379843937b2f38
akritikts <akritikts@gmail.com>
2018-02-15 18:37:51 +0530

    select data added to filter_select

--- filter_select_data.R
+++ filter_select_data.R
@@ -3,4 +3,6 @@ filter_rainfall_data <- function(df)
   # for one SUBDIVISION
   annual_rain_district <- select(df, SUBDIVISION, YEAR, ANNUAL)
   annual_rain_district
+  orrisa_annual_rain <- filter(annual_rain, SUBDIVISION == 'ORISSA')
+  orrisa_annual_rain
 }
